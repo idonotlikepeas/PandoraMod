3ae24c7aeedcff62f7696a74c1895d5854817b8b
Jacob Kantzer <jkantzer@outlook.com>
2016-01-09 12:52:25 -0500

    Set up client/server proxies

---
+++ src/main/java/com/bluestreakgames/pandoramod/CommonProxy.java
@@ -0,0 +1,19 @@
+package com.bluestreakgames.pandoramod;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+/**
+ * Created by jkantzer on 1/9/16.
+ */
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    public void init(FMLInitializationEvent event) {
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
--- src/main/java/com/bluestreakgames/pandoramod/PandoraMod.java
+++ src/main/java/com/bluestreakgames/pandoramod/PandoraMod.java
@@ -2,15 +2,38 @@ package com.bluestreakgames.pandoramod;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
+/**
+ * Created by jkantzer on 1/9/16.
+ */
 @Mod(modid = PandoraMod.MODID, version = PandoraMod.VERSION)
 public class PandoraMod {
     public static final String MODID = "pandoramod";
     public static final String VERSION = "0.1";
 
+    @Instance
+    public static PandoraMod instance = new PandoraMod();
+
+    @SidedProxy(clientSide = "com.bluestreakgames.pandoramod.ClientProxy", serverSide = "com.bluestreakgames.pandoramod.ServerProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        this.proxy.preInit(event);
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // Do stuff
+        this.proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        this.proxy.postInit(event);
     }
 }
