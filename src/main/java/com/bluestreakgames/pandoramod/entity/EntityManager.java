59a0d250b3edc928eb11243a6cdae3927f6da0aa
Jacob Kantzer <jkantzer@outlook.com>
2016-01-10 00:12:27 -0500

    Added helper method to get normal spawn biomes because spawning mobs in Forge is nutty. Creeper Prime spawns randomly and should spawn in dungeons.

--- src/main/java/com/bluestreakgames/pandoramod/entity/EntityManager.java
+++ src/main/java/com/bluestreakgames/pandoramod/entity/EntityManager.java
@@ -5,9 +5,14 @@ import com.bluestreakgames.pandoramod.entity.monster.IPandoraMob;
 import com.bluestreakgames.pandoramod.entity.monster.CreeperPrime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.*;
+import net.minecraftforge.common.DungeonHooks;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import scala.actors.threadpool.Arrays;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -25,7 +30,10 @@ public class EntityManager {
 
         for(IPandoraMob monster : monsters) {
             EntityRegistry.registerModEntity(monster.getEntityClass(), monster.getEntityName(), ++idCounter, PandoraMod.instance, monster.getTrackingRange(), monster.getUpdateFrequency(), monster.getSendsVelocityUpdates(), monster.getEggPrimary(), monster.getEggSecondary());
+            EntityRegistry.addSpawn(monster.getEntityClass(), monster.getSpawnWeightedProb(), monster.getSpawnMin(), monster.getSpawnMax(), EnumCreatureType.MONSTER, monster.getBiomesForSpawn());
         }
+
+        DungeonHooks.addDungeonMob("CreeperPrime", 100);
     }
 
     public static void registerMonsterRenderers() {
@@ -33,5 +41,52 @@ public class EntityManager {
             RenderManager manager = Minecraft.getMinecraft().getRenderManager();
             RenderingRegistry.registerEntityRenderingHandler(monster.getEntityClass(), monster.getEntityRenderer(manager));
         }
+    }
+
+    public static BiomeGenBase[] getStandardBiomes() {
+        // There isn't a better way to get this list, AFAIK. Excludes The End and Hell
+        BiomeGenBase[] standardBiomes = {
+                BiomeGenBase.beach,
+                BiomeGenBase.birchForest,
+                BiomeGenBase.birchForestHills,
+                BiomeGenBase.coldBeach,
+                BiomeGenBase.coldTaiga,
+                BiomeGenBase.coldTaigaHills,
+                BiomeGenBase.deepOcean,
+                BiomeGenBase.desert,
+                BiomeGenBase.desertHills,
+                BiomeGenBase.extremeHills,
+                BiomeGenBase.extremeHillsEdge,
+                BiomeGenBase.extremeHillsPlus,
+                BiomeGenBase.field_180279_ad,
+                BiomeGenBase.forest,
+                BiomeGenBase.forestHills,
+                BiomeGenBase.frozenOcean,
+                BiomeGenBase.frozenRiver,
+                BiomeGenBase.iceMountains,
+                BiomeGenBase.icePlains,
+                BiomeGenBase.jungle,
+                BiomeGenBase.jungleEdge,
+                BiomeGenBase.jungleHills,
+                BiomeGenBase.megaTaiga,
+                BiomeGenBase.megaTaigaHills,
+                BiomeGenBase.mesa,
+                BiomeGenBase.mesaPlateau,
+                BiomeGenBase.mesaPlateau_F,
+                BiomeGenBase.mushroomIsland,
+                BiomeGenBase.mushroomIslandShore,
+                BiomeGenBase.ocean,
+                BiomeGenBase.plains,
+                BiomeGenBase.river,
+                BiomeGenBase.roofedForest,
+                BiomeGenBase.savanna,
+                BiomeGenBase.savannaPlateau,
+                BiomeGenBase.sky,
+                BiomeGenBase.stoneBeach,
+                BiomeGenBase.swampland,
+                BiomeGenBase.taiga,
+                BiomeGenBase.taigaHills
+        };
+        return standardBiomes;
     }
 }
--- src/main/java/com/bluestreakgames/pandoramod/entity/monster/IPandoraMob.java
+++ src/main/java/com/bluestreakgames/pandoramod/entity/monster/IPandoraMob.java
@@ -2,18 +2,25 @@ package com.bluestreakgames.pandoramod.entity.monster;
 
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.world.biome.BiomeGenBase;
 
 /**
  * Created by jkantzer on 1/9/16.
  */
 public interface IPandoraMob {
-    Class<? extends Entity> getEntityClass();
-    Render getEntityRenderer(RenderManager manager);
+    Class<? extends EntityLiving> getEntityClass();
     String getEntityName();
     int getTrackingRange();
     int getUpdateFrequency();
     boolean getSendsVelocityUpdates();
     int getEggPrimary();
     int getEggSecondary();
+
+    BiomeGenBase[] getBiomesForSpawn();
+    int getSpawnWeightedProb();
+    int getSpawnMin();
+    int getSpawnMax();
+
+    Render getEntityRenderer(RenderManager manager);
 }
