53924ed45bacb478222b2d49d232776a225685fe
Jacob Kantzer <jkantzer@outlook.com>
2016-01-09 22:15:25 -0500

    Creeper Prime has increased explosion radius, longer fuse timer, and sets the ground on fire where it walks.

--- src/main/java/com/bluestreakgames/pandoramod/entity/monster/EntityCreeperPrime.java
+++ src/main/java/com/bluestreakgames/pandoramod/entity/monster/EntityCreeperPrime.java
@@ -1,13 +1,62 @@
 package com.bluestreakgames.pandoramod.entity.monster;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by jkantzer on 1/9/16.
  */
 public class EntityCreeperPrime extends EntityCreeper {
+    private int fuseTime = 30;
+    private int explosionRadius = 30;
+
     public EntityCreeperPrime(World worldIn) {
         super(worldIn);
+        setFuseTimeAndExplosionRadius();
+
+        this.isImmuneToFire = true;
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+
+        if(!this.isRiding() && !this.worldObj.isRemote && this.isEntityAlive()) {
+            for (int l = 0; l < 4; ++l)
+            {
+                int i = MathHelper.floor_double(this.posX + (double)((float)(l % 2 * 2 - 1) * 0.25F));
+                int j = MathHelper.floor_double(this.posY);
+                int k = MathHelper.floor_double(this.posZ + (double)((float)(l / 2 % 2 * 2 - 1) * 0.25F));
+
+                Material blockMaterial = this.worldObj.getBlockState(new BlockPos(i, j, k)).getBlock().getMaterial();
+                if (blockMaterial == Material.air) {
+                    this.worldObj.setBlockState(new BlockPos(i, j, k), Blocks.fire.getDefaultState());
+                } else if (blockMaterial == Material.snow) {
+                    this.worldObj.setBlockState(new BlockPos(i, j, k), Blocks.air.getDefaultState());
+                }
+            }
+        }
+    }
+
+    private void setFuseTimeAndExplosionRadius() {
+        try {
+            Field explosionRadius = this.getClass().getSuperclass().getDeclaredField("explosionRadius");
+            explosionRadius.setAccessible(true);
+            explosionRadius.setInt(this, this.explosionRadius);
+
+            Field fuseTime = this.getClass().getSuperclass().getDeclaredField("fuseTime");
+            fuseTime.setAccessible(true);
+            fuseTime.setInt(this, this.fuseTime);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
